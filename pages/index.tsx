a170cca7795eb4fc4aa018da2df47c7e167fc812
Debrup-commits <debruppanda01@gmail.com>
2024-11-12 21:00:44 +0530

    strip down project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,63 @@
-import Layout from "../components/layout"
+import { signIn, signOut, useSession } from "next-auth/react";
+import styles from "./header.module.css"
 
-export default function IndexPage() {
+const Home = () => {
+  const { data: session, status} = useSession(); // Get the session data
+  const loading = status === "loading"
+
   return (
-    <Layout>
-      <h1>NextAuth.js Example</h1>
-      <p>
-        This is an example site to demonstrate how to use{" "}
-        <a href="https://next-auth.js.org">NextAuth.js</a> with {" "}
-        <a href ="https://worldcoin.org/world-id">World ID</a> for authentication.
-      </p>
-    </Layout>
-  )
-}
+    <div className={styles.signedInStatus}>
+        <p
+          className={`nojs-show ${
+            !session && loading ? styles.loading : styles.loaded
+          }`}
+        >
+          {!session && (
+            <>
+              <span className={styles.notSignedInText}>
+                You are not signed in
+              </span>
+              <a
+                href={`/api/auth/signin`}
+                className={styles.buttonPrimary}
+                onClick={(e) => {
+                  e.preventDefault()
+                  signIn("worldcoin") // when worldcoin is the only provider
+                  // signIn() // when there are multiple providers
+                }}
+              >
+                Sign in
+              </a>
+            </>
+          )}
+          {session?.user && (
+            <>
+              {session.user.image && (
+                <span
+                  style={{ backgroundImage: `url('${session.user.image}')` }}
+                  className={styles.avatar}
+                />
+              )}
+              <span className={styles.signedInText}>
+                <small>Signed in as</small>
+                <br />
+                <strong>{session.user.email ?? session.user.name}</strong>
+              </span>
+              <a
+                href={`/api/auth/signout`}
+                className={styles.button}
+                onClick={(e) => {
+                  e.preventDefault()
+                  signOut()
+                }}
+              >
+                Sign out
+              </a>
+            </>
+          )}
+        </p>
+      </div>
+  );
+};
+
+export default Home;
